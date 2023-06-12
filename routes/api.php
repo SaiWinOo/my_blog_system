437f4e79ca461d1ce7c8423dae84d1802cc4f153
saiwinoo <saiwinoo52@gmail.com>
2023-06-12 09:00:03 +0630

    creating chart

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
-use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,7 +22,28 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::get('/users', function () {
-    $users = DB::select('select * from users');
+
+    $start = strtotime(date('M Y', strtotime('-1 Year')));
+    $end = strtotime(date('M Y'));
+    $current = $start;
+    $months = [];
+    $months[date('M Y', $start)] = 0;
+    while ($current < $end) {
+        $next = date('Y-M-01', $current) . '+1 month';
+        $current = strtotime($next);
+        $months[date('M Y', $current)] = 0;
+    }
+    dd($months);
+
+
+    $users = User::query()->orderBy('created_at')->get()->groupBy(function ($val) {
+        return Carbon::parse($val->created_at)->format('M Y');
+    })
+        ->map(function ($eachMonth) {
+            return $eachMonth->count();
+        })
+        ->toArray();
+
     return response()->json([
         'users' => $users
     ]);
